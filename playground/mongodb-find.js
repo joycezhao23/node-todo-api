914118c964403c2c0d3c1b30511091a03f93f447
Xufei Zhao <xzhao@Xufeis-MacBook-Pro.local>
2018-03-04 22:57:44 -0800

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+//const MongoClient = require('mongodb').MongoClient;
+//destructuring below
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Uable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5a9cde8492e10517ab8e0bdd')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // }) ;
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // }) ;
+
+  db.collection('Users').find({name: 'aa'}).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch users', err);
+  })
+
+  //db.close();
+});
